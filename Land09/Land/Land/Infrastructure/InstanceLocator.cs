6e8cc019c46e2ce4adeb794fc739215b106c9dc5
Ramiroqr <ramiroquisper@gmail.com>
2019-09-12 20:53:26 -0500

    Binding Loginpage

---
+++ Land09/Land/Land/Infrastructure/InstanceLocator.cs
@@ -0,0 +1,25 @@
+﻿using Land.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Land.Infrastructure
+{
+    class InstanceLocator
+    {
+        #region Properties
+        public MainViewModel Main
+        {
+            get;
+            set;
+        }
+        #endregion
+
+        #region Constructors
+        public InstanceLocator()
+        {
+            this.Main = new MainViewModel();
+        }
+        #endregion
+    }
+}
