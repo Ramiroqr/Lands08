332019701ebd2f78da5a54906d867258d05c259c
Ramiroqr <ramiroquisper@gmail.com>
2019-09-18 10:32:59 -0500

    BaseViewModel

--- Land09/Land/Land/ViewModels/LoginViewModel.cs
+++ Land09/Land/Land/ViewModels/LoginViewModel.cs
@@ -3,34 +3,48 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Windows.Input;
+using Xamarin.Forms;
 
 namespace Land.ViewModels
 {
-    class LoginViewModel
+    class LoginViewModel : BaseViewModel
     {
+        #region Attributes
+        private string email;
+        private string password;
+        private bool isRunning;
+        private bool isEnabled;
+        #endregion
+
         #region Properties
         public string Email
         {
-            get;
-            set;
+            get { return this.email; }
+            set { SetValue(ref this.email, value); }
         }
 
         public string Password
         {
-            get;
-            set;
+            get { return this.password; }
+            set { SetValue(ref this.password, value); }
         }
 
         public bool IsRunning
         {
-            get;
-            set;
+            get { return this.isRunning; }
+            set { SetValue(ref this.isRunning, value); }
         }
 
         public bool IsRemembered
         {
             get;
             set;
+        }
+
+        public bool IsEnabled
+        {
+            get { return this.isEnabled; }
+            set { SetValue(ref this.isEnabled, value); }
         }
 
         #endregion
@@ -44,9 +58,41 @@ namespace Land.ViewModels
             }
         }
 
-        private void Login()
+        private async void Login()
         {
-            
+            if(string.IsNullOrEmpty(this.Email))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "You must enter an Email...",
+                    "Accept");
+                return;
+            }
+
+            if (string.IsNullOrEmpty(this.Password))
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "You must enter an Password...",
+                    "Accept");
+                return;
+            }
+
+            if(this.Email != "r@gmail.com" || this.Password != "123")
+            {
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    "Password or Email incorrect...",
+                    "Accept");
+                this.Password = string.Empty;
+                return;
+            }
+
+            await Application.Current.MainPage.DisplayAlert(
+                    "Ok",
+                    "Accedo concedido...",
+                    "Accept");
+            return;
         }
         #endregion
 
@@ -54,6 +100,7 @@ namespace Land.ViewModels
         public LoginViewModel()
         {
             this.IsRemembered = true;
+            this.IsEnabled = true;
         }
         #endregion
     }
