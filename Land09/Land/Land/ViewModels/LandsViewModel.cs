67d703623429fef5a11f9b67eb3c08e1b7ad90be
Ramiroqr <ramiroquisper@gmail.com>
2019-09-23 07:14:08 -0500

    LandsViewMod

--- Land09/Land/Land/ViewModels/LandsViewModel.cs
+++ Land09/Land/Land/ViewModels/LandsViewModel.cs
@@ -1,10 +1,97 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Text;
+using System.Windows.Input;
+using GalaSoft.MvvmLight.Command;
+using Land.Models;
+using Land.Services;
+using Xamarin.Forms;
 
 namespace Land.ViewModels
 {
-    class LandsViewModel
+    class LandsViewModel : BaseViewModel
     {
+        #region Services
+        private ApiService apiService;
+        #endregion
+
+        #region Attributes
+        private ObservableCollection<Land1> lands;
+        private bool isRefreshing;
+        #endregion
+
+        #region Properties
+        public ObservableCollection<Land1> Lands
+        {
+            get { return this.lands; }
+            set { SetValue(ref this.lands, value); }
+        }
+
+        public bool IsRefreshing
+        {
+            get { return this.isRefreshing; }
+            set { SetValue(ref this.isRefreshing, value); }
+        }
+        #endregion
+
+        #region Commands
+        public ICommand RefreshCommand
+        {
+            get
+            {
+                return new RelayCommand(LoadLands);
+            }
+        }
+        #endregion
+
+        #region Constructors
+        public LandsViewModel()
+        {
+            this.apiService = new ApiService();
+            this.LoadLands();
+        }
+        #endregion
+
+        #region Methos
+        private async void LoadLands()
+        {
+            this.IsRefreshing = true;
+            var connection = await this.apiService.CheckConnection();
+            if(!connection.IsSuccess)
+            {
+                this.isRefreshing = false;
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    connection.Message,
+                    "Accept");
+                await Application.Current.MainPage.Navigation.PopAsync();
+            }
+
+            var response = await this.apiService.GetList<Land1>(
+                "http://restcountries.eu",
+                "/rest",
+                "/v2/all");
+
+            if(!response.IsSuccess)
+            {
+                this.isRefreshing = false;
+                await Application.Current.MainPage.DisplayAlert(
+                    "Error",
+                    response.Message,
+                    "Accept");
+                await Application.Current.MainPage.Navigation.PopAsync();
+                return;
+            }
+
+            var list = (List<Land1>)response.Result;
+            this.Lands = new ObservableCollection<Land1>(list);
+            this.IsRefreshing = false;
+        }
+        #endregion
+
+
+
+
     }
 }
